67f5be959b30ed1c9a47487446dcfacac429e793
Matt Thompson <matt@echobind.com>
2022-04-04 10:52:49 -0400

    app controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def user_is_logged_in?
+    unless session[:oktastate]
+      print('user is not logged in')
+      redirect_to user_oktaoauth_omniauth_authorize_path
+    end
+  end
+
+  def after_sign_in_path_for(_resource)
+    request.env['omniauth.origin'] || root_path
+  end
 end
