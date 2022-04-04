c46c08a39423e63d01c9ab170c5017b2856c4cf3
Matt Thompson <matt@echobind.com>
2022-04-04 10:51:24 -0400

    User Controller/Model setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 
   def destroy
+    session[:oktastate] = nil
+    @current_user = session[:oktastate]
+    @session = session[:oktastate]
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'pages/home'
   get 'pages/account'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 end
